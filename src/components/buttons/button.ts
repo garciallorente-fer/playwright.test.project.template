7068b33f92a931367d7a30c64045cf9ae5ec2616
Fernando Garcia Llorente <garciallorente.fer@gmail.com>
2022-02-22 20:08:13 +0000

    Fix 1.1

--- src/components/buttons/button.ts
+++ src/components/buttons/button.ts
@@ -4,7 +4,7 @@ import { Component } from '../component';
 export class Button extends Component {
 
   public async click(options?: { noWaitAfter: boolean }): Promise<void> {
-    await this.locator.click({ noWaitAfter: options?.noWaitAfter });
+    await this.locator.click(options);
   }
 
 }
